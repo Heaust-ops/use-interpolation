a009b82fb59c13d946b83918e63f38ad638fbee7
heaust <heaust.azure@gmail.com>
2023-02-07 05:13:21 +0530

    remove dead code

--- src/hooks/use-interpolation.ts
+++ src/hooks/use-interpolation.ts
@@ -1,15 +1,5 @@
 import { useState, useEffect } from "react";
 
-type Tuple<T, N extends number> = N extends N
-  ? number extends N
-    ? T[]
-    : _TupleOf<T, N, []>
-  : never;
-
-type _TupleOf<T, N extends number, R extends unknown[]> = R["length"] extends N
-  ? R
-  : _TupleOf<T, N, [T, ...R]>;
-
 type Tensor = Array<number | Tensor>
 
 const lerp = <T extends number | Tensor>(
